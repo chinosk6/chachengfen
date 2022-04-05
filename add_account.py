cca54ebd014916f48bc6a9d1f4800ac3eae3889a
chinosk <74499927+chinosk114514@users.noreply.github.com>
2022-04-05 15:01:45 +0800

    优化sql语句写法
    不建议直接拼接sql语句, 最好进行参数化

--- add_account.py
+++ add_account.py
@@ -4,13 +4,13 @@ uid = int(input("请输入用户uid:"))
 SESSDATA = input("请输入SESSDATA:")
 bili_jct = input("请输入bili_jct:")
 following_count = int(input("请输入关注数:"))
-full_cookie = input("请输入完整关注:")
+full_cookie = input("请输入完整cookie:")
 
 conn = sqlite3.connect(f"./dd_query/cookies/bili_dd_account.db")
 cursor = conn.cursor()
-sql = 'insert into dds (uid, SESSDATA,bili_jct,full_cookie,following_count) values (%d, "%s", "%s", "%s", %d)' % (uid, SESSDATA, bili_jct, full_cookie, following_count)
 
-cursor.execute(sql)
+cursor.execute('insert into dds (uid, SESSDATA, bili_jct, full_cookie, following_count) values (?, ?, ?, ?, ?)',
+               (uid, SESSDATA, bili_jct, full_cookie, following_count))
 conn.commit()
 cursor.close()
 conn.close()
